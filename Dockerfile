a919e588277413b3d111e44cd7d7a6bb13f64b63
Audrey Simonne <audrey_simonne@ultimatesoftware.com>
2020-02-01 11:14:06 -0500

    Adds mongo to save post data and get GET data to/from mongo

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,14 @@ FROM node:11.6-slim
 
 WORKDIR /usr/src
 
-COPY package.json /usr/src/package.json
+COPY package.json /usr/src/
 
+# TODO: should use yarn since project uses yarn
+# Can use base yarn image with node runtime image
 RUN npm install
 
 COPY index.js /usr/src/index.js
+COPY mongo/ /usr/src/
 
 ENV NODE_ENV=production
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -8,26 +9,63 @@ const app = express();
 // Application configuration comes from the environment
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+const MONGO_DB = process.env.MONGO_DB || 'swamphacks';
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
 
 app.use(logger('tiny'));
 
+// Try to connect to Mongo
+const client = new MongoClient(MONGO_URI, {useNewUrlParser: true});
+
+var db;
+
+client.connect()
+    .then(() => {
+        db = client.db(MONGO_DB);
+    })
+    .catch((err) => {
+        console.log("Could not connect to mongo");
+        console.log(err);
+        client.close();
+        process.exit(1)
+    });
+
 // TODO: Capture body parser error message and make it nicer
 app.use(bodyParser.json());
 
 app.get('/swamphacks/v1/randopando', (req, res) => {
-    res.send({
-        "message": "beepboop it's a test",
-    });
+    db.collection('randopando').find({}).toArray()
+        .then((data) => {
+            console.log("data is:");
+            console.log(data);
+            res.send(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.json({"message": "merp retrieval error"});
+        });
 })
 
 app.post('/swamphacks/v1/randopando', (req, res) => {
     console.log(req.body);
-    res.json({
-        "message": "you posted a thing",
-    });
+
+    db.collection('randopando').insertOne(req.body)
+        .then(() => {
+            res.json({
+                "message": "you posted a thing",
+            });
+        })
+        .catch((err) => {
+            console.log("oops there was an error");
+            res.status(500);
+            res.json({"message": "merp insertion error"});
+        });
+
+    
 });
 
 // Set the content type of the response
@@ -60,6 +98,8 @@ const expressServer = app.listen(PORT, () => {
 function gracefulShutdown(server) {
     console.log("App was killed.");
     console.log("Generally do your graceful shutdown things here");
+    // Notice we close the connection!
+    client.close();
     server.close(() => {
         console.log("beepboop")
     });
